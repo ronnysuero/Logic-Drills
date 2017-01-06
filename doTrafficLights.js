df1b276c67d0cde1de5976af51eb5831b02dff76
Depto. de Desarrollo MOPC <e.encarnacion@mopc.gob.do>
2017-01-06 16:00:45 -0400

    first commit

---
+++ doTrafficLights.js
@@ -0,0 +1,56 @@
+function doTrafficLights() {
+  var activeLight = getActiveLight();
+  // your code will replace this call
+  // to `console.log()`
+  if(activeLight == "red")
+      return turnRed();
+  else if(activeLight == "green")
+      return turnGreen();
+  else if(activeLight == "yellow")
+      return turnYellow();
+      
+
+}
+
+
+/* From here down, you are not expected to 
+   understand.... for now :)  
+   
+   
+   Nothing to see here!
+   
+*/
+
+
+function turnOffLights() {
+  $('.traffic-light').removeClass('yellow-on red-on green-on');
+}
+
+function turnGreen() {
+  turnOffLights();
+  $('.green-light').addClass('green-on');
+}
+
+function turnYellow() {
+  turnOffLights();
+  $('.yellow-light').addClass('yellow-on');
+}
+
+function turnRed() {
+  turnOffLights();
+  $('.red-light').addClass('red-on');
+}
+
+function getActiveLight() {
+  return (['red', 'green', 'yellow'])[Math.floor(Math.random() * 3)];
+}
+
+function handleClicks() {
+  $('.js-control-lights').click(function() {
+    doTrafficLights();
+  });
+}
+
+$(function() {
+  handleClicks();
+})
